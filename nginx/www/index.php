2ca6d911a6e8cd5f659db0ece30cef100ffa5b5f
Denis Tokarev <denistok@xicod.com>
2021-09-04 13:03:38 +0300

    add nginx deployment

---
+++ nginx/www/index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>nginx</title>
+</head>
+<body><h1>
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+echo '[' . $_SERVER['HTTP_HOST'] . ']';
+?>
+</h1>
+</body></html>
